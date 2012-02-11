84314f426aea1a638478f1ee1ceeac675422f58a
Ken Cochrane <kencochrane@gmail.com>
2012-02-11 15:25:47 -0500

    initial checkin version, can open and edit files, no ReST yet. split screen window.

--- Bowie/Document.h
+++ Bowie/Document.h
@@ -3,11 +3,18 @@
 //  Bowie
 //
 //  Created by Ken Cochrane on 2/11/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Zanbow Inc. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
 @interface Document : NSDocument
+{
+    IBOutlet NSTextView *textView;
+    NSAttributedString *mString;
+}
+
+- (NSAttributedString *) string;
+- (void) setString: (NSAttributedString *) value;
 
 @end
--- Bowie/Document.m
+++ Bowie/Document.m
@@ -14,10 +14,24 @@
 {
     self = [super init];
     if (self) {
-        // Add your subclass-specific initialization here.
-        // If an error occurs here, return nil.
+        if (mString == nil) {
+            mString = [[NSAttributedString alloc] initWithString:@""];
+        }
     }
     return self;
+}
+
+- (NSAttributedString *) string { return mString; }
+
+- (void) setString: (NSAttributedString *) newValue {
+    if (mString != newValue) {
+        mString = [newValue copy];
+    }
+}
+
+- (void) textDidChange: (NSNotification *) notification
+{
+    [self setString: [textView textStorage]];
 }
 
 - (NSString *)windowNibName
@@ -31,29 +45,35 @@
 {
     [super windowControllerDidLoadNib:aController];
     // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    
+    if ([self string] != nil) {
+        [[textView textStorage] setAttributedString: [self string]];
+    }
 }
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-    /*
-     Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    */
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return nil;
+    NSData *data;
+    [self setString:[textView textStorage]];
+    NSMutableDictionary *dict = [NSDictionary dictionaryWithObject:NSRTFTextDocumentType
+                                                            forKey:NSDocumentTypeDocumentAttribute];
+    [textView breakUndoCoalescing];
+    data = [[self string] dataFromRange:NSMakeRange(0, [[self string] length])
+                     documentAttributes:dict error:outError];
+    return data;
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
-    /*
-    Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    */
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return YES;
+    BOOL readSuccess = NO;
+    NSAttributedString *fileContents = [[NSAttributedString alloc]
+                                        initWithData:data options:NULL documentAttributes:NULL
+                                        error:outError];
+    if (fileContents) {
+        readSuccess = YES;
+        [self setString:fileContents];
+    }
+    return readSuccess;
 }
 
 + (BOOL)autosavesInPlace
